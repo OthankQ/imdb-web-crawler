89937c0565ec6ed92f4f752011ddd9baf2b30685
Ryan <kim1885@purdue.edu>
2019-05-01 18:54:41 -0400

    change arrow function to pre ES6 function declaration

--- server.js
+++ server.js
@@ -7,14 +7,48 @@ const app = express();
 
 app.get('/scrape', (req, res) => {
   // url we will scrape from
-  const url = 'https://www.imdb.com/showtimes/title/tt4154796?ref_=sh_ov_tt';
+  url = 'https://www.imdb.com/title/tt4154796/?ref_=shtt_ov_tt';
 
   request(url, (error, response, html) => {
     if (!error) {
-      const $ = cheerio.load(html);
-      const title, release, rating;
-      const json = { title: "", release: "", rating: "" }
+      var $ = cheerio.load(html);
+      var title, release, rating;
+      var json = { title: '', release: '', rating: '' };
+
+      $('.title_wrapper').filter(function() {
+        const data = $(this);
+        title = data
+          .children()
+          .first()
+          .text()
+          .trim();
+        release = data
+          .children()
+          .last()
+          .children()
+          .last()
+          .text()
+          .trim();
+
+        json.title = title;
+        json.release = release;
+      });
+
+      $('.ratingValue').filter(function() {
+        const data = $(this);
+        rating = data.text().trim();
+
+        json.rating = rating;
+      });
     }
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), err => {
+      if (!err) {
+        console.log('File successfully written!');
+      }
+    });
+
+    res.send('Check your console!');
   });
 });
 
